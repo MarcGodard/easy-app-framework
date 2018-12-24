6f47a5253b8a7d405a216bad76c6a8391ceba923
Marc Godard <me@marcgodard.com>
2018-12-23 22:08:30 -0500

    Add feathers client to redux store

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,33 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Framework7StateKernel, framework7Reducer, syncFramework7WithStore } from 'framework7-redux'
 import middleware from '../middleware'
 
+// feathers imports
+import io from 'socket.io-client'
+import feathers from '@feathersjs/client'
+import reduxifyServices from 'feathers-redux'
+
+// Reducer imports
 import loginReducer from '../reducers/LoginReducer'
 import formReducer from '../reducers/FormReducer'
+
+// setup feathers
+const socket = io('http://localhost:3030');
+const feathersClient = feathers()
+
+feathersClient.configure(feathers.socketio(socket))
+feathersClient.configure(feathers.authentication({
+  storage: window.localStorage
+}))
+
+// Create Redux actions and reducers for Feathers services
+const services = reduxifyServices(feathersClient, ['users'])
 
 export const stateKernel = new Framework7StateKernel()
 
 export const store = createStore(
   combineReducers({
     framework7: framework7Reducer,
+    users: services.users.reducer,
     login: loginReducer,
     form: formReducer
   }),
